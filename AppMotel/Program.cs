fb8ef4973c1ecd35bbe45b27352ba85278db79ea
Annisa Uswa Sufia <annisauswasufia@gmail.com>
2022-09-03 06:44:38 +0700

    first commit

---
+++ AppMotel/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppMotel
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Menu instance = new Menu();
+            instance.menu();
+        }
+    }
+}
---
+++ AppMotel/exit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppMotel
+{
+    internal class Exit
+    {
+        public void exit()
+        {
+            Environment.Exit(0);
+        }
+    }
+}
